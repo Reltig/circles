34c751cfbaae9ec5978f293721455a0d598f504f
m1nus0ne <Voldemar7455@Gmail.com>
2021-04-20 08:36:10 +0500

    ver 1.0

--- Point.py
+++ Point.py
@@ -1,21 +1,26 @@
 import pygame
-from config import WHITE
-
+from config import WHITE, LINEAR_SPEED
+from math import cos, sin
 
 class Point(object):
-    def __init__(self, x, y, velocity, center, radius, color, connectedPoint = None):
-        self.x = x
-        self.y = y
-        self.velocity = velocity
+    def __init__(self, cord, center, width, track_radius, color=WHITE, connectedPoint=None, angle_velocty=0):
+        self.track_radius = None
+        self.cord = cord
+        self.angle_velocity = angle_velocty
         self.center = center
-        self.radius = radius
+        self.width = width
         self.color = color
         self.connectedPoint = connectedPoint
-
+        self.track_radius = track_radius
+        self.t = 0
     def move(self, deltaTime):
-        pass
+        self.t += deltaTime
+        self.cord = (self.center[0] + self.track_radius * cos(self.angle_velocity*self.t),
+                     self.center[1] + self.track_radius * sin(self.angle_velocity*self.t))
+
+
 
     def draw(self, screen):
-        pygame.draw.circle(screen, WHITE, (self.x, self.y), self.radius)
+        pygame.draw.circle(screen, WHITE, self.cord, self.width)
         if self.connectedPoint is not None:
-            pygame.draw.aaline(screen, WHITE, (self.x, self.y), (self.connectedPoint.x, self.connectedPoint.y))
+            pygame.draw.aaline(screen, WHITE, self.cord, self.connectedPoint.cord,2)
--- circle.py
+++ circle.py
@@ -1,28 +1,45 @@
 import pygame
 import sys
 from config import *
-import Point
+from Point import Point
 
 clock = pygame.time.Clock()
-sc = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+sc = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT),)
 isRunning = True
 
 
-def pointsFabric(count, center, r):
+def pointsFabric(count, center, r, width):
     points = []
     step = r / count
     x0, y0 = center[0], center[1]
-    points.append(Point(x0 + step, y0, ))
+    ANGLE_VELOCITY = LINEAR_SPEED/step
+    points.append(Point(cord = (x0 + step, y0),center =  (x0, y0),
+                        width = width, track_radius = step,
+                        angle_velocty = ANGLE_VELOCITY))
+
     for i in range(2, count + 1):
-        points.append(Point(x, y, ))
+        print(ANGLE_VELOCITY)
+        ANGLE_VELOCITY = LINEAR_SPEED/(i * step)
+        points.append(Point(cord = (x0 + step * i, y0),
+                            center = (x0, y0),track_radius= i * step,width= width,
+                            color= WHITE, connectedPoint= points[i - 2], angle_velocty= ANGLE_VELOCITY))
+
     return points
 
 
-points = pointsFabric(POINTS_COUNT, (WIN_HEIGHT // 2, WIN_HEIGHT // 2), RADIUS)
+points = pointsFabric(POINTS_COUNT, (WIN_HEIGHT // 2, WIN_HEIGHT // 2), RADIUS, WIDTH)
+clock.tick(FPS)
 while isRunning:
+    sc.fill(BLACK)
+    pygame.draw.circle(sc,WHITE, (WIN_HEIGHT // 2, WIN_HEIGHT // 2), WIDTH)
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             isRunning = False
             sys.exit()
-    sc.fill(BLACK)
+    for point in points:
+        point.draw(sc)
+
+        point.move(clock.tick(FPS))
+
+    pygame.display.flip()
     clock.tick(FPS)
